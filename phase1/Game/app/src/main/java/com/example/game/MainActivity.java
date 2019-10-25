067c63cf933e171627e76d86f089683094f7be0f
Benjamin <benjamin.zhuo@mail.utoronto.ca>
2019-10-24 20:53:21 -0400

    Added styles, manifests, layout for tapioca launcher game.

--- phase1/Game/app/src/main/java/com/example/game/MainActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.game;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.game.TapiocaLauncher.GameActivity;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
     }
 }
