f661608b20c5083ad7c5e195a6daf342e0e3b530
Christine Lee <chrstn.lee@mail.utoronto.ca>
2019-10-27 19:59:14 -0400

    Added an abstract Tile class to represent tiles in the Tiles game.

---
+++ phase1/Game/app/src/main/java/com/example/game/TilesGame/Tile.java
@@ -0,0 +1,74 @@
+package com.example.game.TilesGame;
+
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+abstract class Tile {
+
+  /**
+   * The width of a tile.
+   */
+  static int width = Resources.getSystem().getDisplayMetrics().widthPixels / 4;
+
+  /**
+   * The height of a tile.
+   */
+  static int height = Resources.getSystem().getDisplayMetrics().heightPixels / 4;
+
+  /**
+   * The x-coordinate of this tile.
+   */
+  int x;
+
+  /**
+   * The y-coordinate of this tile.
+   */
+  int y;
+
+  Paint paintRect = new Paint();
+
+  /**
+   * Construct a tile at location (x, y).
+   *
+   * @param x: the x-coordinate of this tile.
+   * @param y: the y-coordinate of this tile.
+   */
+  Tile(int x, int y) {
+
+    this.x = x;
+    this.y = y;
+  }
+
+  public static int getWidth() {
+    return width;
+  }
+
+  public static int getHeight() {
+    return height;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  void draw(Canvas canvas) {
+    // Draw border of tile.
+    paintRect.setStyle(Paint.Style.STROKE);
+    paintRect.setColor(Color.rgb(200, 200, 200)); // LIGHT GRAY
+    canvas.drawRect(x, y, x + width, y + height, paintRect);
+  }
+}
