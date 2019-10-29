dc351c2fea94fc0fe0047b3a47958e1205c57e04
chiang67 <sean.chiang@mail.utoronto.ca>
2019-10-29 09:15:46 -0400

    Updated the visual componenet of View to draw the current score and steps on the screen.

--- phase1/Game/app/src/main/java/com/example/game/MazeGame/VisualView.java
+++ phase1/Game/app/src/main/java/com/example/game/MazeGame/VisualView.java
@@ -2,6 +2,7 @@ package com.example.game.MazeGame;
 
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.SurfaceHolder;
 
@@ -12,13 +13,16 @@ import java.util.Observer;
 public class VisualView implements Observer {
 
   private Cell[][] grid; // Representation of the Maze grid object.
-  private Paint paint;
+  private Paint backgroundPaint, textPaint;
   private Tile tile;
   private Background background;
   private SurfaceHolder surfaceHolder;
 
   // Maximum x and y positions of the screen.
   private int maxScreenX, maxscreenY;
+
+  private int score;
+  private int numSteps;
 
   /**
    * @param maxScreenX - maximum x position of the screen
@@ -34,10 +38,15 @@ public class VisualView implements Observer {
 
     this.surfaceHolder = surfaceHolder;
 
-    this.paint = new Paint();
+    this.backgroundPaint = new Paint();
+
+    // Set colors and size of text.
+    this.textPaint = new Paint();
+    textPaint.setColor(Color.GREEN);
+    textPaint.setTextSize(32);
   }
 
-  /** Draws the Tiles of the Maze. */
+  /** Draws the UI of the Maze. */
   void draw() {
 
     if (surfaceHolder.getSurface().isValid()) {
@@ -45,34 +54,49 @@ public class VisualView implements Observer {
       Canvas canvas = surfaceHolder.lockCanvas();
 
       // Draw the background first.
-      canvas.drawBitmap(background.getBackground(), background.getX(), background.getY(), paint);
-
-      // Maze is drawn so that it is centered on the screen.
-
-      // Following two lines represents the x and y position of the first Tile of the Maze,
-      // i.e. grid[0][0].
-      int topLeftTileX = (maxScreenX - grid[0].length * tile.getSideLength()) / 2;
-      int topLeftTileY = (maxscreenY - grid.length * tile.getSideLength()) / 2;
-
-      // Draw every Tile of the Maze. An offset of a Tile's side length ensures that each
-      // Tile is drawn right next to each other.
-      for (int i = 0; i < grid.length; i++) {
-        for (int j = 0; j < grid[i].length; j++) {
-          canvas.drawBitmap(
-              tile.getTile(grid[i][j]),
-              topLeftTileX + (j * tile.getSideLength()),
-              topLeftTileY + (i * tile.getSideLength()),
-              paint);
-        }
-      }
+      canvas.drawBitmap(
+          background.getBackground(), background.getX(), background.getY(), backgroundPaint);
+      drawTiles(canvas);
+      drawText(canvas);
 
       surfaceHolder.unlockCanvasAndPost(canvas);
+    }
+  }
+
+  /** Draws the text of the Maze. */
+  private void drawText(Canvas canvas) {
+    canvas.drawText(String.format("Score: %d", score), maxScreenX - 250, 50, textPaint);
+    canvas.drawText(String.format("Steps: %d", numSteps), 115, 50, textPaint);
+  }
+
+  /** Draws the Tiles of the Maze. */
+  private void drawTiles(Canvas canvas) {
+    // Maze is drawn so that it is centered on the screen.
+
+    // Following two lines represents the x and y position of the first Tile of the Maze,
+    // i.e. grid[0][0].
+    int topLeftTileX = (maxScreenX - grid[0].length * tile.getSideLength()) / 2;
+    int topLeftTileY = (maxscreenY - grid.length * tile.getSideLength()) / 2;
+
+    // Draw every Tile of the Maze. An offset of a Tile's side length ensures that each
+    // Tile is drawn right next to each other.
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        canvas.drawBitmap(
+            tile.getTile(grid[i][j]),
+            topLeftTileX + (j * tile.getSideLength()),
+            topLeftTileY + (i * tile.getSideLength()),
+            backgroundPaint);
+      }
     }
   }
 
   /** @param o most recent representation of the Maze. */
   @Override
   public void update(Observable observable, Object o) {
-    this.grid = (Cell[][]) o;
+    NewGameState newGameState = (NewGameState) o;
+    grid = newGameState.getGrid();
+    score = newGameState.getScore();
+    numSteps = newGameState.getNumSteps();
   }
 }
