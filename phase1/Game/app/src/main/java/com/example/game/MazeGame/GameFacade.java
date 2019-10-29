2c1fd3d20ae2126d03e63bafae4e9d684cf662ac
chiang67 <sean.chiang@mail.utoronto.ca>
2019-10-29 09:14:51 -0400

    Updated the Observable behaviour of GameFacade to return additional information about the score.

--- phase1/Game/app/src/main/java/com/example/game/MazeGame/GameFacade.java
+++ phase1/Game/app/src/main/java/com/example/game/MazeGame/GameFacade.java
@@ -6,48 +6,47 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Observable;
 
-//TODO: return a Cell 2d array representation of game; method name getGrid()
+// TODO: return a Cell 2d array representation of game; method name getGrid()
 
 public class GameFacade extends Observable {
 
-    private Player player;
-    private Maze maze;
+  private Player player;
+  private Maze maze;
 
-    private Map<Movement, Pair<Integer, Integer>> movementMap = new HashMap<>();
+  private Map<Movement, Pair<Integer, Integer>> movementMap = new HashMap<>();
 
-    // Assume player init position is in a valid position in the map
-    public GameFacade(Player player, Maze maze) {
-        this.player = player;
-        this.maze = maze;
-    }
+  // Assume player init position is in a valid position in the map
+  public GameFacade(Player player, Maze maze) {
+    this.player = player;
+    this.maze = maze;
+  }
 
-    Player getPlayer() {
-        return player;
-    }
+  Player getPlayer() {
+    return player;
+  }
 
-    Maze getMaze() {
-        return maze;
-    }
+  Maze getMaze() {
+    return maze;
+  }
 
-    void update(Pair<Integer , Integer> movement_vector) {
-        // moves player
-        int xDisplacement =  movement_vector.first;
-        int yDisplacement =  movement_vector.second;
-        player.displace(xDisplacement, yDisplacement);
+  void update(Pair<Integer, Integer> movement_vector) {
+    // moves player
+    int xDisplacement = movement_vector.first;
+    int yDisplacement = movement_vector.second;
+    player.displace(xDisplacement, yDisplacement);
 
-        setChanged();
-        // Need to put a 2d array of Cells into notifyObservers parameters
-        notifyObservers(getCellrepresentation());
+    setChanged();
+    // Need to supply an updated representation of the new Maze and score to be drawn.
+    NewGameState newGameState =
+        new NewGameState(getCellrepresentation(), player.getScore(), player.getNumSteps());
+    notifyObservers(newGameState);
+  }
 
-
-    }
-
-    private Cell [][] getCellrepresentation(){
-        // Must use a deep copy to prevent aliasing when replacing a Cell Tile with a
-        // Player Cell Tile.
-        Cell [][] representation = maze.getGridDeepCopy();
-        representation[player.getPos()[1]][player.getPos()[0]] = Cell.PLAYER;
-        return representation;
-    }
-
+  private Cell[][] getCellrepresentation() {
+    // Must use a deep copy to prevent aliasing when replacing a Cell Tile with a
+    // Player Cell Tile.
+    Cell[][] representation = maze.getGridDeepCopy();
+    representation[player.getPos()[1]][player.getPos()[0]] = Cell.PLAYER;
+    return representation;
+  }
 }
