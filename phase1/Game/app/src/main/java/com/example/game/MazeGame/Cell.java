ec5575ad3048a3501f2c7572596ab08a84bc9582
chiang67 <sean.chiang@mail.utoronto.ca>
2019-10-29 10:45:36 -0400

    Added a drawable resource for an exit tile and updated Cell and Tile to represent it.

--- phase1/Game/app/src/main/java/com/example/game/MazeGame/Cell.java
+++ phase1/Game/app/src/main/java/com/example/game/MazeGame/Cell.java
@@ -3,6 +3,8 @@ package com.example.game.MazeGame;
 public enum Cell {
     FLOOR,
     WALL,
+    EXIT,
     PLAYER,
+    PLAYER_AT_EXIT,
     ITEMS
 }
--- phase1/Game/app/src/main/java/com/example/game/MazeGame/Tile.java
+++ phase1/Game/app/src/main/java/com/example/game/MazeGame/Tile.java
@@ -6,57 +6,65 @@ import android.graphics.BitmapFactory;
 
 import com.example.game.R;
 
-/**
- * Class representing the tiles of the maze. A tile is a wall, a floor, or a player.
- */
+/** Class representing the tiles of the maze. A tile is a wall, a floor, or a player. */
 public class Tile {
 
-    private Bitmap wall, floor, player;
+  private Bitmap wall, floor, player, playerAtExit;
 
-    // Side length of any Tile bitmap.
-    private int sideLength;
+  // Side length of any Tile bitmap.
+  private int sideLength;
 
-    public Tile(Resources res) {
+  public Tile(Resources res) {
 
-        Bitmap tmpWall = BitmapFactory.decodeResource(res, R.drawable.brick_wall);
-        Bitmap tmpfloor = BitmapFactory.decodeResource(res, R.drawable.floor);
-        Bitmap tmpCharacter = BitmapFactory.decodeResource(res, R.drawable.character);
+    Bitmap tmpWall = BitmapFactory.decodeResource(res, R.drawable.brick_wall);
+    Bitmap tmpfloor = BitmapFactory.decodeResource(res, R.drawable.floor);
+    Bitmap tmpPlayer = BitmapFactory.decodeResource(res, R.drawable.character);
+    Bitmap tmpCharacterAtExit = BitmapFactory.decodeResource(res, R.drawable.character_at_exit);
 
-        // Reference tile for the side length of any tile.
-        Bitmap ref = tmpWall;
+    // Reference tile for the side length of any tile.
+    Bitmap ref = tmpWall;
 
-        // Rescale the dimensions of the image by this factor.
-        int SCALE_FACTOR = 8;
+    // Rescale the dimensions of the image by this factor.
+    int SCALE_FACTOR = 8;
 
-        this.sideLength = ref.getWidth() / SCALE_FACTOR;
+    this.sideLength = ref.getWidth() / SCALE_FACTOR;
 
-        // Rescale the wall and floor bitmaps to fit on the phone screen.
-        this.wall = Bitmap.createScaledBitmap(tmpWall, sideLength, sideLength, false);
-        this.floor = Bitmap.createScaledBitmap(tmpfloor, sideLength, sideLength, false);
-        this.player = Bitmap.createScaledBitmap(tmpCharacter, sideLength, sideLength, false);
+    // Rescale the wall and floor bitmaps to fit on the phone screen.
+    this.wall = Bitmap.createScaledBitmap(tmpWall, sideLength, sideLength, false);
+    this.floor = Bitmap.createScaledBitmap(tmpfloor, sideLength, sideLength, false);
+    this.player = Bitmap.createScaledBitmap(tmpPlayer, sideLength, sideLength, false);
+    this.playerAtExit =
+        Bitmap.createScaledBitmap(tmpCharacterAtExit, sideLength, sideLength, false);
+  }
+
+  /**
+   * Exit Tile is represented as a normal floor Tile until the player is at its location.
+   *
+   * @param tile - integer representing the tile to be returned
+   */
+  Bitmap getTile(Cell tile) {
+    switch (tile) {
+      case FLOOR:
+        return floor;
+
+      case WALL:
+        return wall;
+
+      case PLAYER:
+        return player;
+
+      case EXIT:
+        return floor;
+
+      case PLAYER_AT_EXIT:
+        return playerAtExit;
+
+      default:
+        return wall;
     }
+  }
 
-    /**
-     * @param tile - integer representing the tile to be returned
-     */
-    Bitmap getTile(Cell tile) {
-        switch (tile) {
-            case FLOOR:
-                return floor;
-
-            case WALL:
-                return wall;
-
-            case PLAYER:
-                return player;
-
-            default:
-                return wall;
-        }
-    }
-
-    int getSideLength() {
-        return sideLength;
-    }
-
+  int getSideLength() {
+    return sideLength;
+  }
 }
