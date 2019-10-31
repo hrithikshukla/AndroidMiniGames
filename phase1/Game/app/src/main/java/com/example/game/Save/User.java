a664dfa84bc3b70ef34eea7a612e592be8ccdbdd
chiang67 <sean.chiang@mail.utoronto.ca>
2019-10-31 12:53:42 -0400

    Implemented a backend saving/login system that uses a .json file. Used Google's GSON library to facilitate this implementation.

---
+++ phase1/Game/app/src/main/java/com/example/game/Save/User.java
@@ -0,0 +1,31 @@
+package com.example.game.Save;
+
+/** Class representing a User. */
+class User {
+
+  private String username;
+  private String password;
+  /** userData is the player's saved information about our games. */
+  private UserData userData;
+
+  public User(String username, String password, UserData userData) {
+    this.username = username;
+    this.password = password;
+    this.userData = userData;
+  }
+
+  /** Getter for username. */
+  String getUsername() {
+    return username;
+  }
+
+  /** Getter for userdata. */
+  UserData getUserData() {
+    return userData;
+  }
+
+  /** Returns whether given password matches the User's password. */
+  boolean checkPassword(String inputPassword) {
+    return password.equals(inputPassword);
+  }
+}
