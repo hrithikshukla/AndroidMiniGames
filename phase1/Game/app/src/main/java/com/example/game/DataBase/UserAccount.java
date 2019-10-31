72082ed902ada2300ec9f365ad10f83cca0226bc
Ren Jie Kan <renjie.kan@mail.utoronto.ca>
2019-10-31 11:57:13 -0400

    Fixed primary key nonnullable error for UserScores

--- phase1/Game/app/src/main/java/com/example/game/DataBase/UserAccount.java
+++ phase1/Game/app/src/main/java/com/example/game/DataBase/UserAccount.java
@@ -1,25 +1,26 @@
-//package com.example.game.DataBase;
-//
-//
-//import androidx.room.Entity;
-//import androidx.room.PrimaryKey;
-//
-//@Entity(tableName = "Accounts_table")
-//public class UserAccount {
-//    @PrimaryKey
-//    private String userName;
-//    private String password;
-//
-//    public UserAccount(String userName, String password) {
-//        this.userName = userName;
-//        this.password = password;
-//    }
-//
-//    public String getUserName() {
-//        return userName;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//}
+package com.example.game.DataBase;
+
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Accounts_table")
+public class UserAccount {
+    @PrimaryKey
+    @NonNull private String userName;
+    private String password;
+
+    public UserAccount(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
