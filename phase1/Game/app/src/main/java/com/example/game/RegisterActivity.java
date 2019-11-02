9252e5247da45f78bf17f86aeddd9a132540facf
chiang67 <seam.chiang@mail.utoronto.ca>
2019-11-01 22:01:44 -0400

    Updated RegisterActivity to only register a user given valid inputs i.e. non-empty username and password. The uesr is informed via toast messages if their registration failed.

--- phase1/Game/app/src/main/java/com/example/game/RegisterActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.game;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,33 +16,44 @@ import com.example.game.Save.AccountsManager;
 
 public class RegisterActivity extends AppCompatActivity {
 
-  EditText username;
-  EditText password;
-  Button registerButton;
-  AccountsManager accountsManager;
+    EditText username;
+    EditText password;
+    Button registerButton;
+    AccountsManager accountsManager;
 
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_register);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
 
-    username = (EditText) findViewById(R.id.username);
-    password = (EditText) findViewById(R.id.password);
-    registerButton = (Button) findViewById(R.id.button2);
-    this.accountsManager = (AccountsManager) getIntent().getSerializableExtra("AccountsManager");
-    System.out.println("m");
-  }
-
-  public void register(View view) {
-    username = (EditText) findViewById(R.id.username);
-    password = (EditText) findViewById(R.id.password);
-    if (accountsManager.createUser(
-        username.getText().toString().trim(), password.getText().toString().trim())) {
-      Intent loginIntent = new Intent(RegisterActivity.this, com.example.game.LoginActivity.class);
-      startActivity(loginIntent);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        registerButton = (Button) findViewById(R.id.button2);
+        this.accountsManager = (AccountsManager) getIntent().getSerializableExtra("AccountsManager");
     }
-    else {
-      Toast.makeText(getApplicationContext(), "Invalid Registration", Toast.LENGTH_LONG).show();
+
+    public void register(View view) {
+
+        String usernameString = username.getText().toString().trim();
+        String passwordString = password.getText().toString().trim();
+
+        // Empty usernames are illegal an the user is informed.
+        if (usernameString.equals("")) {
+            Toast.makeText(getApplicationContext(), "Input a username", Toast.LENGTH_LONG).show();
+        }
+
+        // Empty passwords are illegal and the user is informed.
+        else if (passwordString.equals("")) {
+            Toast.makeText(getApplicationContext(), "Input a password", Toast.LENGTH_LONG).show();
+        } else if (accountsManager.createUser(usernameString, passwordString)) {
+            // Informs the user that their account is created.
+            Toast.makeText(getApplicationContext(), String.format("New user %s created", usernameString), Toast.LENGTH_LONG).show();
+            Intent loginIntent = new Intent(RegisterActivity.this, com.example.game.LoginActivity.class);
+            startActivity(loginIntent);
+        }
+        // Informs the user that their input username is already an existing user.
+        else {
+            Toast.makeText(getApplicationContext(), "User already exists", Toast.LENGTH_LONG).show();
+        }
     }
-  }
 }
