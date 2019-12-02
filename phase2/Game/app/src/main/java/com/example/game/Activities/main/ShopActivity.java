9318d8095b83ba1d30a462fc557efe2ea0f64dba
Ren Jie Kan <renjie.kan@mail.utoronto.ca>
2019-12-01 20:27:05 -0500

    remove livedatat for user coins in shop

--- phase2/Game/app/src/main/java/com/example/game/Activities/main/ShopActivity.java
+++ phase2/Game/app/src/main/java/com/example/game/Activities/main/ShopActivity.java
@@ -31,7 +31,7 @@ public class ShopActivity extends AppCompatActivity {
   private List<ImageView> images = new ArrayList<>();
   // Checks if back button is pressed
   protected OnBackPressedListener onBackPressedListener;
-    private List<Integer> ownedChars;
+  private List<Integer> ownedChars;
   private String username;
   TextView userCoins;
 
@@ -70,107 +70,107 @@ public class ShopActivity extends AppCompatActivity {
 
     // Find and add images to images list and set ID tags and set character prices
     ImageView worm = findViewById(R.id.char_worm);
-      worm.setTag(R.id.num, R.drawable.char_worm);
+    worm.setTag(R.id.num, R.drawable.char_worm);
     worm.setTag(R.id.price, 21);
-      worm.setTag(R.id.id, R.id.char_worm);
+    worm.setTag(R.id.id, R.id.char_worm);
     images.add(worm);
     ImageView bird = findViewById(R.id.char_bird);
-      bird.setTag(R.id.num, R.drawable.char_bird_blue);
+    bird.setTag(R.id.num, R.drawable.char_bird_blue);
     bird.setTag(R.id.price, 42);
-      bird.setTag(R.id.id, R.id.char_bird);
+    bird.setTag(R.id.id, R.id.char_bird);
     images.add(bird);
     ImageView spider = findViewById(R.id.char_spider);
-      spider.setTag(R.id.num, R.drawable.char_spider);
+    spider.setTag(R.id.num, R.drawable.char_spider);
     spider.setTag(R.id.price, 50);
-      spider.setTag(R.id.id, R.id.char_spider);
+    spider.setTag(R.id.id, R.id.char_spider);
     images.add(spider);
     ImageView dog = findViewById(R.id.char_dog);
-      dog.setTag(R.id.num, R.drawable.char_dog);
+    dog.setTag(R.id.num, R.drawable.char_dog);
     dog.setTag(R.id.price, 69);
-      dog.setTag(R.id.id, R.id.char_dog);
+    dog.setTag(R.id.id, R.id.char_dog);
     images.add(dog);
     ImageView tree = findViewById(R.id.char_tree);
-      tree.setTag(R.id.num, R.drawable.char_tree);
+    tree.setTag(R.id.num, R.drawable.char_tree);
     tree.setTag(R.id.price, 80);
-      tree.setTag(R.id.id, R.id.char_tree);
+    tree.setTag(R.id.id, R.id.char_tree);
     images.add(tree);
     ImageView astro = findViewById(R.id.char_astro);
-      astro.setTag(R.id.num, R.drawable.char_astro);
+    astro.setTag(R.id.num, R.drawable.char_astro);
     astro.setTag(R.id.price, 80);
-      astro.setTag(R.id.id, R.id.char_astro);
+    astro.setTag(R.id.id, R.id.char_astro);
     images.add(astro);
     ImageView alien = findViewById(R.id.char_alien);
-      alien.setTag(R.id.num, R.drawable.char_alien_dark);
+    alien.setTag(R.id.num, R.drawable.char_alien_dark);
     alien.setTag(R.id.price, 90);
-      alien.setTag(R.id.id, R.id.char_astro);
+    alien.setTag(R.id.id, R.id.char_astro);
     images.add(alien);
     ImageView monster = findViewById(R.id.char_monster);
-      monster.setTag(R.id.num, R.drawable.char_monster_red);
+    monster.setTag(R.id.num, R.drawable.char_monster_red);
     monster.setTag(R.id.price, 100);
-      monster.setTag(R.id.id, R.id.char_monster);
+    monster.setTag(R.id.id, R.id.char_monster);
     images.add(monster);
     ImageView bat = findViewById(R.id.char_bat);
-      bat.setTag(R.id.num, R.drawable.char_bat);
+    bat.setTag(R.id.num, R.drawable.char_bat);
     bat.setTag(R.id.price, 150);
-      bat.setTag(R.id.id, R.id.char_bat);
+    bat.setTag(R.id.id, R.id.char_bat);
     images.add(bat);
     ImageView king = findViewById(R.id.char_king);
-      king.setTag(R.id.num, R.drawable.char_king);
+    king.setTag(R.id.num, R.drawable.char_king);
     king.setTag(R.id.price, 151);
-      king.setTag(R.id.id, R.id.char_king);
+    king.setTag(R.id.id, R.id.char_king);
     images.add(king);
     ImageView summoner = findViewById(R.id.char_summoner);
-      summoner.setTag(R.id.num, R.drawable.char_summoner);
+    summoner.setTag(R.id.num, R.drawable.char_summoner);
     summoner.setTag(R.id.price, 1000);
-      summoner.setTag(R.id.id, R.id.char_summoner);
+    summoner.setTag(R.id.id, R.id.char_summoner);
     images.add(summoner);
     ImageView viking1 = findViewById(R.id.char_viking1);
-      viking1.setTag(R.id.num, R.drawable.char_viking_1);
+    viking1.setTag(R.id.num, R.drawable.char_viking_1);
     viking1.setTag(R.id.price, 200);
-      viking1.setTag(R.id.id, R.id.char_viking1);
+    viking1.setTag(R.id.id, R.id.char_viking1);
     images.add(viking1);
     ImageView viking2 = findViewById(R.id.char_viking2);
-      viking2.setTag(R.id.num, R.drawable.char_viking_2);
+    viking2.setTag(R.id.num, R.drawable.char_viking_2);
     viking2.setTag(R.id.price, 200);
-      viking2.setTag(R.id.id, R.id.char_viking2);
+    viking2.setTag(R.id.id, R.id.char_viking2);
     images.add(viking2);
     ImageView viking3 = findViewById(R.id.char_viking3);
-      viking3.setTag(R.id.num, R.drawable.char_viking_3);
+    viking3.setTag(R.id.num, R.drawable.char_viking_3);
     viking3.setTag(R.id.price, 200);
-      viking3.setTag(R.id.id, R.id.char_viking3);
+    viking3.setTag(R.id.id, R.id.char_viking3);
     images.add(viking3);
     ImageView wizard = findViewById(R.id.char_wizard);
     wizard.setTag(R.id.price, 300);
-      wizard.setTag(R.id.num, R.drawable.char_wizard);
-      wizard.setTag(R.id.id, R.id.char_wizard);
+    wizard.setTag(R.id.num, R.drawable.char_wizard);
+    wizard.setTag(R.id.id, R.id.char_wizard);
     images.add(wizard);
     ImageView archer = findViewById(R.id.char_archer);
-      archer.setTag(R.id.num, R.drawable.char_archer);
+    archer.setTag(R.id.num, R.drawable.char_archer);
     archer.setTag(R.id.price, 300);
-      archer.setTag(R.id.id, R.id.char_archer);
+    archer.setTag(R.id.id, R.id.char_archer);
     images.add(archer);
     ImageView knight = findViewById(R.id.char_knight);
-      knight.setTag(R.id.num, R.drawable.char_knight);
+    knight.setTag(R.id.num, R.drawable.char_knight);
     knight.setTag(R.id.price, 350);
-      knight.setTag(R.id.id, R.id.char_knight);
+    knight.setTag(R.id.id, R.id.char_knight);
     images.add(knight);
     ImageView samurai = findViewById(R.id.char_samurai);
-      samurai.setTag(R.id.num, R.drawable.char_samurai);
+    samurai.setTag(R.id.num, R.drawable.char_samurai);
     samurai.setTag(R.id.price, 420);
-      samurai.setTag(R.id.id, R.id.char_samurai);
+    samurai.setTag(R.id.id, R.id.char_samurai);
     images.add(samurai);
     ImageView shogun = findViewById(R.id.char_shogun);
-      shogun.setTag(R.id.num, R.drawable.char_shogun);
+    shogun.setTag(R.id.num, R.drawable.char_shogun);
     shogun.setTag(R.id.price, 450);
-      shogun.setTag(R.id.id, R.id.char_shogun);
+    shogun.setTag(R.id.id, R.id.char_shogun);
     images.add(shogun);
 
-//    System.out.println(ownedChars.getValue() == null);
+    //    System.out.println(ownedChars.getValue() == null);
 
     // Grey out the characters that are not owned and add click viewers for characters
     for (final ImageView img : images) {
       grayOut(img);
-        System.out.println(img.getId());
+      System.out.println(img.getId());
       img.setOnClickListener(
           new View.OnClickListener() {
             @Override
@@ -184,11 +184,12 @@ public class ShopActivity extends AppCompatActivity {
               FragmentTransaction fragmentTransaction = fm.beginTransaction();
               fragmentTransaction
                   .replace(
-                          R.id.frame, new SpriteFragment(
-                                  (int) img.getTag(R.id.num),
-                                  (int) img.getTag(R.id.price),
-                                  (int) img.getTag(R.id.id),
-                                  username))
+                      R.id.frame,
+                      new SpriteFragment(
+                          (int) img.getTag(R.id.num),
+                          (int) img.getTag(R.id.price),
+                          (int) img.getTag(R.id.id),
+                          username))
                   .addToBackStack(null)
                   .commit();
             }
@@ -196,14 +197,9 @@ public class ShopActivity extends AppCompatActivity {
     }
 
     userCoins = findViewById(R.id.userAmount);
-    LiveData<Integer> userAmount = uR.getUserAmountTest();
-    userAmount.observe(this, new Observer<Integer>() {
-      @Override
-      public void onChanged(Integer integer) {
-          userCoins.setText(userCoins.getText() + ": " + integer);
+    int userAmount = uR.getUserAmount();
 
-      }
-    });
+    userCoins.setText(userCoins.getText() + ": " + userAmount);
   }
 
   @Override
@@ -214,25 +210,23 @@ public class ShopActivity extends AppCompatActivity {
 
   // Code from https://gist.github.com/nisrulz/3078eaa6357d6f5c0051
   private void grayOut(ImageView img) {
-//     if not owned grey them out
-//     Don't mind the horrible time complexity
-      if (ownedChars == null) {
-          ColorMatrix matrix = new ColorMatrix();
-          matrix.setSaturation(0);
-          ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
-          img.setColorFilter(filter);
+    //     if not owned grey them out
+    //     Don't mind the horrible time complexity
+    if (ownedChars == null) {
+      ColorMatrix matrix = new ColorMatrix();
+      matrix.setSaturation(0);
+      ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+      img.setColorFilter(filter);
 
+    } else if (!ownedChars.contains(img.getTag(R.id.id))) {
+      ColorMatrix matrix = new ColorMatrix();
+      matrix.setSaturation(0);
+      ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+      img.setColorFilter(filter);
 
-      } else if (!ownedChars.contains(img.getTag(R.id.id))) {
-          ColorMatrix matrix = new ColorMatrix();
-          matrix.setSaturation(0);
-          ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
-          img.setColorFilter(filter);
-
-      } else { // no filter
-          img.setColorFilter(null);
-
-      }
+    } else { // no filter
+      img.setColorFilter(null);
+    }
   }
 
   public void setOnBackPressedListener(OnBackPressedListener onBackPressedListener) {
